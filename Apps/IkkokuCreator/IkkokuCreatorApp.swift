0e379bab2b0cdf50540edeb266a6d5855ed182ca
Fran <90626364+grabbiel@users.noreply.github.com>
2026-08-18 11:41:40 -0700

    gitgnore

---
+++ Apps/IkkokuCreator/IkkokuCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IkkokuCreatorApp.swift
+//  IkkokuCreator
+//
+//  Created by rumpology on 8/18/26.
+//
+
+import SwiftUI
+
+@main
+struct IkkokuCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
